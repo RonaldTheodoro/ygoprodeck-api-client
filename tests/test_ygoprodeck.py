16aeb08cba28155bd9d249bf26863854cd122898
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-12-06 23:29:35 -0200

    Add test_validate_level_rank_invalid_value and test_validate_level_rank_out_of_range

--- tests/test_ygoprodeck.py
+++ tests/test_ygoprodeck.py
@@ -13,3 +13,11 @@ def test_validate_type(client):
         client.get_cards(type_='not valid type')
 
 
+def test_validate_level_rank_invalid_value(client):
+    with pytest.raises(ygoprodeck.exceptions.LevelOrRankInvalid):
+        client.get_cards(level='not a number')
+
+
+def test_validate_level_rank_out_of_range(client):
+    with pytest.raises(ygoprodeck.exceptions.LevelOrRankInvalid):
+        client.get_cards(level=15)
