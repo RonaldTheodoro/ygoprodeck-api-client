1e02c559912247e3747f4f15f35e8f7a24892317
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2019-03-24 17:02:13 -0300

    Add ban lists tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -41,17 +41,38 @@ def test_model_linkmarkers(card):
         card.linkmarkers = 'north'
 
 
-def test_model_linkmarker_list(card):
-    card.linkmarkers = 'left,right,bottom,'
-    assert card.linkmarkers == ['left', 'right', 'bottom',]
-
-
 def test_model_race(card):
     with pytest.raises(ygoprodeck.exceptions.RaceInvalid):
         card.race = 'yokai'
+
+
+def test_model_ban_tcg(card):
+    with pytest.raises(ygoprodeck.exceptions.BanlistStatusInvalid):
+        card.ban_tcg = 'status'
+
+
+def test_model_ban_ocg(card):
+    with pytest.raises(ygoprodeck.exceptions.BanlistStatusInvalid):
+        card.ban_ocg = 'status'
+
+
+def test_model_ban_goat(card):
+    with pytest.raises(ygoprodeck.exceptions.BanlistStatusInvalid):
+        card.ban_goat = 'status'
+
+
+def test_model_linkmarker_list(card):
+    card.linkmarkers = 'left,right,bottom,'
+    assert card.linkmarkers == ['left', 'right', 'bottom', ]
 
 
 def test_model_set_tag(card):
     card.set_tag = 'YS12-EN037,YS11-EN039,LODT-EN063,5DS2-EN037,'
     set_tag = ['YS12-EN037', 'YS11-EN039', 'LODT-EN063', '5DS2-EN037', ]
     assert card.set_tag == set_tag
+
+
+def test_model_setcode(card):
+    card.setcode = 'Raging Tempest,2017 Mega-Tin Mega Pack,'
+    setcode = ['Raging Tempest', '2017 Mega-Tin Mega Pack']
+    assert card.setcode == setcode
