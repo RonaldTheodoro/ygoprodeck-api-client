afa222225ed15e26430a906b755a1b536b36db93
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2019-03-24 17:50:01 -0300

    Updade setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ long_description = open('README.md').read()
 
 
 setuptools.setup(
-    name='DB YGOPro Deck API client',
+    name='ygoprodeck-api-client',
     version='0.0.1',
     description='A simple client to access the https://db.ygoprodeck.com API',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     author='Ronald Theodoro',
     author_email='ronald.silva4@fatec.sp.gov.br',
@@ -19,6 +20,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
     ],
 )
 
