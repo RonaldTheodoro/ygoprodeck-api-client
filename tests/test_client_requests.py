2fe46c3f5acd4c47c3aa257751096b4693cc91d7
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-10-02 11:37:17 -0300

    Update tests

--- tests/test_client_requests.py
+++ tests/test_client_requests.py
@@ -26,27 +26,63 @@ def client(betamax_session):
 def test_get_cards(client):
     cards = client.get_cards(name='Meteor B. Dragon')
     expected = {
-        'id': '90660762',
-        'name': 'Meteor B. Dragon',
+        'id': 90660762,
+        'name': 'Meteor Black Dragon',
         'type': 'Fusion Monster',
-        'desc': '"Red-Eyes B. Dragon" + "Meteor Dragon"',
-        'atk': '3500',
-        'def': '2000',
-        'level': '8',
+        'desc': '"Red-Eyes Black Dragon" + "Meteor Dragon"',
+        'atk': 3500,
+        'def': 2000,
+        'level': 8,
         'race': 'Dragon',
         'attribute': 'FIRE',
         'archetype': 'Red-Eyes',
-        'set_tag': 'PRC1-EN004,',
-        'setcode': 'Premium Collection Tin,',
-        'image_url': (
-            'https://storage.googleapis.com/ygoprodeck.com/pics/90660762.jpg'
-        ),
-        'image_url_small': (
-            'https://storage.googleapis.com/ygoprodeck.com/'
-            'pics_small/90660762.jpg'
-        )
+        'card_sets': [
+            {
+                'set_name': 'Legendary Duelists: Season 1',
+                'set_code': 'LDS1-EN013',
+                'set_rarity': 'Common',
+                'set_rarity_code': '(C)',
+                'set_price': '1.06'
+            },
+            {
+                'set_name': 'Premium Collection Tin',
+                'set_code': 'PRC1-EN004',
+                'set_rarity': 'Super Rare',
+                'set_rarity_code': '(SR)',
+                'set_price': '4.16'
+            },
+            {
+                'set_name': 'Speed Duel: Arena of Lost Souls',
+                'set_code': 'SBLS-EN013',
+                'set_rarity': 'Super Rare',
+                'set_rarity_code': '(SR)',
+                'set_price': '3.59'
+            }
+        ],
+        'card_images': [
+            {
+                'id': 90660762,
+                'image_url': (
+                    'https://storage.googleapis.com'
+                    '/ygoprodeck.com/pics/90660762.jpg'
+                ),
+                'image_url_small': (
+                    'https://storage.googleapis.com'
+                    '/ygoprodeck.com/pics_small/90660762.jpg'
+                )
+            }
+        ],
+        'card_prices': [
+            {
+                'cardmarket_price': '0.14',
+                'tcgplayer_price': '1.73',
+                'ebay_price': '9.95',
+                'amazon_price': '34.99',
+                'coolstuffinc_price': '0.25'
+            }
+        ]
     }
-    assert cards[0][0] == expected
+    assert cards['data'][0] == expected
 
 
 def test_get_cards_invalid(client):
