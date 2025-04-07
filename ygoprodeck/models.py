8f9c55a2b066e1a8f621d8fa88ebfa334aa26df9
Ronald Theodoro <ronaldtheodoro@gmail.com>
2025-04-07 00:17:18 -0300

    refactor models

--- ygoprodeck/models.py
+++ ygoprodeck/models.py
@@ -15,6 +15,32 @@ from . import validators
 
 
 @dataclass
+class CardSet:
+    set_name: str
+    set_code: str
+    set_rarity: str
+    set_rarity_code: str
+    set_price: str
+
+
+@dataclass
+class CardImage:
+    id: int
+    image_url: str
+    image_url_small: str
+    image_url_cropped: str
+
+
+@dataclass
+class CardPrice:
+    cardmarket_price: str
+    tcgplayer_price: str
+    ebay_price: str
+    amazon_price: str
+    coolstuffinc_price: str
+
+
+@dataclass
 class Card:
     """Model that represent a card instance"""
 
@@ -26,83 +52,91 @@ class Card:
     def_: Optional[int] = None
     _level: Optional[int] = field(default=None)
     _attribute: Optional[str] = field(default=None)
-    _scale: Optional[str] = field(default=None)
+    _scale: Optional[int] = field(default=None)
     _race: Optional[str] = field(default=None)
     archetype: Optional[str] = None
-    _link: Optional[str] = field(default=None)
-    _linkmarkers: Optional[str] = field(default=None)
-    _set_tag: Optional[str] = field(default=None)
-    _setcode: Optional[str] = field(default=None)
+
+    _link: Optional[int] = field(default=None)
+    _linkmarkers: Optional[list[str]] = field(default=None)
+
+    _set_tag: Optional[list[str]] = field(default=None)
+    _setcode: Optional[list[str]] = field(default=None)
+
     image_url: Optional[str] = None
     image_url_small: Optional[str] = None
+
     _ban_tcg: Optional[str] = field(default=None)
     _ban_ocg: Optional[str] = field(default=None)
     _ban_goat: Optional[str] = field(default=None)
 
+    _card_sets: Optional[list[CardSet]] = None
+    _card_images: Optional[list[CardImage]] = None
+    _card_prices: Optional[list[CardPrice]] = None
+
     @property
-    def type_(self):
+    def type_(self) -> Optional[str]:
         return self._type
 
     @type_.setter
-    def type_(self, type_):
+    def type_(self, type_: str):
         validators.card_type(type_)
         self._type = type_
 
     @property
-    def level(self):
+    def level(self) -> Optional[int]:
         return self._level
 
     @level.setter
-    def level(self, level):
+    def level(self, level: int):
         if level is not None:
             validators.card_level_rank(level)
         self._level = level
 
     @property
-    def attribute(self):
+    def attribute(self) -> Optional[str]:
         return self._attribute
 
     @attribute.setter
-    def attribute(self, attribute):
+    def attribute(self, attribute: str):
         if attribute is not None:
             validators.card_attribute(attribute)
         self._attribute = attribute
 
     @property
-    def scale(self):
+    def scale(self) -> Optional[int]:
         return self._scale
 
     @scale.setter
-    def scale(self, scale):
+    def scale(self, scale: int):
         if scale is not None:
             validators.card_pendulum_scale(scale)
         self._scale = scale
 
     @property
-    def race(self):
+    def race(self) -> Optional[str]:
         return self._race
 
     @race.setter
-    def race(self, race):
+    def race(self, race: str):
         validators.card_race(race)
         self._race = race
 
     @property
-    def link(self):
+    def link(self) -> Optional[int]:
         return self._link
 
     @link.setter
-    def link(self, link):
+    def link(self, link: int):
         if link is not None:
             validators.card_link(link)
         self._link = link
 
     @property
-    def linkmarkers(self):
+    def linkmarkers(self) -> Optional[list[str]]:
         return self._linkmarkers
 
     @linkmarkers.setter
-    def linkmarkers(self, linkmarkers):
+    def linkmarkers(self, linkmarkers: str):
         if linkmarkers is not None:
             linkmarkers = linkmarkers.split(",")
             if linkmarkers[-1] == "":
@@ -112,53 +146,77 @@ class Card:
         self._linkmarkers = linkmarkers
 
     @property
-    def set_tag(self):
+    def set_tag(self) -> Optional[list[str]]:
         return self._set_tag
 
     @set_tag.setter
-    def set_tag(self, set_tag):
+    def set_tag(self, set_tag: str):
         set_tag = set_tag.split(",")
         if set_tag[-1] == "":
             set_tag.pop(-1)
         self._set_tag = set_tag
 
     @property
-    def setcode(self):
+    def setcode(self) -> Optional[list[str]]:
         return self._setcode
 
     @setcode.setter
-    def setcode(self, setcode):
+    def setcode(self, setcode: str):
         setcode = setcode.split(",")
         if setcode[-1] == "":
             setcode.pop(-1)
         self._setcode = setcode
 
     @property
-    def ban_tcg(self):
+    def ban_tcg(self) -> Optional[str]:
         return self._ban_tcg
 
     @ban_tcg.setter
-    def ban_tcg(self, ban_tcg):
+    def ban_tcg(self, ban_tcg: str):
         if ban_tcg is not None:
             validators.card_banlist_status(ban_tcg)
         self._ban_tcg = ban_tcg
 
     @property
-    def ban_ocg(self):
+    def ban_ocg(self: str) -> Optional[str]:
         return self._ban_ocg
 
     @ban_ocg.setter
-    def ban_ocg(self, ban_ocg):
+    def ban_ocg(self, ban_ocg: str) -> Optional[str]:
         if ban_ocg is not None:
             validators.card_banlist_status(ban_ocg)
         self._ban_ocg = ban_ocg
 
     @property
-    def ban_goat(self):
+    def ban_goat(self) -> Optional[str]:
         return self._ban_goat
 
     @ban_goat.setter
-    def ban_goat(self, ban_goat):
+    def ban_goat(self, ban_goat: str):
         if ban_goat is not None:
             validators.card_banlist_status(ban_goat)
         self._ban_goat = ban_goat
+
+    @property
+    def card_sets(self) -> Optional[list[CardSet]]:
+        return self._card_sets
+
+    @card_sets.setter
+    def card_sets(self, card_sets: list):
+        self._card_sets = [CardSet(**c) for c in card_sets]
+
+    @property
+    def card_images(self) -> Optional[list[CardImage]]:
+        return self._card_images
+
+    @card_images.setter
+    def card_images(self, card_images: list):
+        self._card_images = [CardImage(**c) for c in card_images]
+
+    @property
+    def card_prices(self) -> Optional[list[CardPrice]]:
+        return self._card_prices
+
+    @card_prices.setter
+    def card_prices(self, card_prices: list):
+        self._card_prices = [CardPrice(**c) for c in card_prices]
